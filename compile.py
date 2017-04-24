dad72d92e32bf5da35209d214f0878c404e32a06
gkrishnan724 <gkrishnan724@gmail.com>
2017-04-24 20:37:05 +0530

    Make outputs come in a docfile

--- compile.py
+++ compile.py
@@ -1,16 +1,17 @@
 import os
-
-outdir = "Outputs/"
-for name in os.listdir("/home/gopal/lab7/"):
+i = 1
+for name in os.listdir("/home/gopal/lab7/"): #put the name of the directory where the c programs are present
         if os.path.isfile(name):
-                out = outdir+name.replace(".c","")
-                display = "cat "+name+" >> outfile.txt "
-                compileit = "gcc "+name+" -lmo"+out
-                execute = "./" + out+" >> outfile.txt "
+             if ".c" in name:
+                out = name.replace(".c","")
+                display = "cat "+name+" >> outfile.odt " #outfile.odt is where the outputs will be stored
+                compileit = "gcc "+name+" -lm -o "+out
+                execute = "./" + out+" >> outfile.odt"
+                os.system("echo " + str(i) + ".  >> outfile.odt")
                 os.system(display)
-                os.system("echo '\n'")
+                os.system("echo '\n' >> outfile.odt")
                 os.system(compileit)
-                os.system("echo 'output:'")
+                os.system("echo 'output:' >> outfile.odt")
                 os.system(execute)
-                os.system("echo '\n' ")
-              
+                os.system("echo '\n' >> outfile.odt")
+                i += 1
